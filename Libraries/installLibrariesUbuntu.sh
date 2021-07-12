9aac4b96c5e2bb5437fe0cdc29dd7360012dd1ad
ananiac <ananiac@ideavat.com>
2021-07-12 22:49:21 +0530

    batchforlibraries_1

---
+++ Libraries/installLibrariesUbuntu.sh
@@ -0,0 +1,8 @@
+# execute this file in ubuntu with sh i.e sh installLibrariesUbuntu.sh
+cd ..
+chmod 755 installLibrariesUbuntu.sh
+sudo pip install robotframework-SeleniumLibrary
+sudo pip install robotframework-requests
+sudo pip install robotframework-jsonlibrary
+sudo pip install robotframework-sshlibrary
+sudo pip install -u robotframework-pabot
