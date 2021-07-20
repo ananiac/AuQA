3d9702bc0592185c2502f199c34f7306f7d25c00
ananiac <ananiac@ideavat.com>
2021-07-20 12:44:45 +0530

    AuQA-4-multipleconfig-3

---
+++ Configurations/config64.py
@@ -0,0 +1,31 @@
+#Automation setup configuration
+url_vx="https://10.252.9.64/vems/vx.php"
+url_cx="https://10.252.9.64/vems/cx.php"
+graphql_base_url="https://10.252.9.64/api"
+
+
+#use headlessmode to run in ubuntu
+browser="headlesschrome"
+#use this to run on windows
+#browser="chrome"
+
+
+#Credentials to login
+ui_username="ideavat"
+ui_password="Crick3t!"
+#Other username and password is vx/vx,admin/admin
+
+
+#API Headers
+content_type="application/json"
+write_api_token="vd2R6aDXgz5e9zkmf7TfZzqN"
+query_api_token="bZVd43sS4Y9WZfZ7du3kY6ST"
+
+
+#Terminal Configuration
+host_ip="10.252.9.64"
+ssh_username="fc"
+ssh_password="Id3aV@tAcc3ss" #Id3aV@tAcc3ss
+
+#Execution command
+#robot --variable environment:config64 Testcases/basicHotAbsoluteGuardTest.robot
