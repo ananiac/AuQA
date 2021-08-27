2f3cdfd2f3c5f8dbb0a2b439ddb24df0a641d0d9
ananiac <ananiac@ideavat.com>
2021-08-27 22:43:41 +0530

    fixforuielementonubuntu

--- PageObjects/siteEditorHomePage.py
+++ PageObjects/siteEditorHomePage.py
@@ -1,7 +1,7 @@
 #Site Editor Web Elements
 banner="css:#banner"
-tools_button="xpath=//span[contains(text(),'Tools')]"
-configs_option_in_tools="xpath=//span[contains(text(),'Configs')][1]"
+tools_button="xpath=//div[1]/div/div[1]/div[2]/div/a[1]/span/span/span[2]"
+configs_option_in_tools="xpath=//div[12]/div/div/div[2]/div/div[4]/a/span"
 load_template_in_config_popup="xpath=//span[contains(text(),'Load Template')]"
 overwrite_checkbox="xpath=//div[13]/div[2]/div/div/div/div[1]/div/div/div[3]/div[1]/div/span/input"
 template_dropbox_picker="xpath=//div[13]/div[2]/div/div/div/div[1]/div/div/div[1]/div[1]/div/div[2]"
