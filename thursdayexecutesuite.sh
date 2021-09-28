b1f3393bc718c23ad873f95d9341cd0cd08cab99
ananiac <ananiac@ideavat.com>
2021-09-28 12:40:33 +0530

    updated message and addred path to thursday suite

--- thursdayexecutesuite.sh
+++ thursdayexecutesuite.sh
@@ -1,19 +1,47 @@
 # test suite for execution scheduled on thursday using cronjob
 # Can be executed manually from AuQA folder using - sh thursdayexecutesuite.sh
+#path="E:/Ideavat/AutomatedQA/AuQA/Testcases" # /home/fc/automation/AuQA/Testcases
+
+full_path=$(realpath $0)
+echo $full_path
+
+dir_path=$(dirname $full_path)
+echo $dir_path
+
+tc_path=$dir_path/Testcases
+echo $tc_path
+
+rp_path=$dir_path/Reports
+echo $rp_path
 
     ##Used the below for Ubuntu machine
 if [ `ps -ef | grep pabot | wc -l` -lt 2 ];  then
-    echo "pabot is not running-suite execution started"
-    cd /home/fc/automation/AuQA
-    sudo pabot --pabotlib -d Reports/cleanReports --output cleanReports.xml --variable environment:config118  /home/fc/automation/AuQA/Testcases/cleanReports.robot || echo $(date +%F_%H:%M:%S)- cleanReports failed >> Reports/executionLog.txt 2>&1
-    sudo pabot --pabotlib --processes 2 --name "Guard1_General-test_$(date +%F_%H:%M:%S)" --reporttitle "BasicHotAbsoluteGuard" --outputdir Reports --output basichotGuard.xml --variable environment:config118  -v groupname:General-test -T /home/fc/automation/AuQA/Testcases/basicHotAbsoluteGuardTest.robot /home/fc/automation/AuQA/Testcases/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- BasicHotAbsoluteGuard/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
-    sudo pabot --pabotlib --processes 2 --name "Guard2_Imputes-test_$(date +%F_%H:%M:%S)" --reporttitle "DeadSensorGuard" --outputdir Reports --output deadSensorGuard.xml --variable environment:config118  -v groupname:Imputes-test -T /home/fc/automation/AuQA/Testcases/deadSensorGuardTest.robot /home/fc/automation/AuQA/Testcases/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- DeadSensorGuard/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
-    sudo pabot --pabotlib --processes 2 --name "Guard3_General-test_$(date +%F_%H:%M:%S)" --reporttitle "HotGuard" --outputdir Reports --output hotGuard.xml --variable environment:config118  -v groupname:General-test -T /home/fc/automation/AuQA/Testcases/hotGuardTest.robot  /home/fc/automation/AuQA/Testcases/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- HotGuard/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
-    sudo rebot  --name "GuardTest" --reporttitle "Test performed on environment 118" --outputdir Reports --output output.xml /home/fc/automation/AuQA/Reports/basichotGuard.xml /home/fc/automation/AuQA/Reports/deadSensorGuard.xml /home/fc/automation/AuQA/Reports/hotGuard.xml 2>&1 | sudo tee -a Reports/executionLog.txt
-    sudo pabot --pabotlib -d Reports/cleanReports --output moveReports.xml --variable environment:config118 /home/fc/automation/AuQA/Testcases/moveReports.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- moveReports failed >> Reports/executionLog.txt 2>&1
+    echo "No automated tests are running so starting the test execution"
+    cd $dir_path
+    pabot --pabotlib -d Reports/cleanReports --output cleanReports.xml --variable environment:config118  $tc_path/cleanReports.robot
+    pabot --pabotlib --processes 2 --name "Guard1_General-test_$(date +%F_%H:%M:%S)" --reporttitle "BasicHotAbsoluteGuard" --outputdir Reports --output basichotGuard.xml --variable environment:config118  -v groupname:General-test -T $tc_path/basicHotAbsoluteGuardTest.robot $tc_path/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- BasicHotAbsoluteGuard/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
+    pabot --pabotlib --processes 2 --name "Guard2_Imputes-test_$(date +%F_%H:%M:%S)" --reporttitle "DeadSensorGuard" --outputdir Reports --output deadSensorGuard.xml --variable environment:config118  -v groupname:Imputes-test -T $tc_path/deadSensorGuardTest.robot $tc_path/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- DeadSensorGuard/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
+    pabot --pabotlib --processes 2 --name "Guard3_General-test_$(date +%F_%H:%M:%S)" --reporttitle "HotGuard" --outputdir Reports --output hotGuard.xml --variable environment:config118  -v groupname:General-test -T $tc_path/hotGuardTest.robot  $tc_path/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- HotGuard/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
+    rebot  --name "GuardTest" --reporttitle "Test performed on environment 118" --outputdir Reports --output output.xml $rp_path/basichotGuard.xml $rp_path/deadSensorGuard.xml $rp_path/hotGuard.xml 2>&1 | sudo tee -a Reports/executionLog.txt
+    pabot --pabotlib -d Reports/cleanReports --output moveReports.xml --variable environment:config118 $tc_path/moveReports.robot
+    echo "Automated test execution completed"
   else
-    echo "pabot is already running"
+    echo "Automated test are running so the test execution is aborted"
  fi
+
+
+# if [ `ps -ef | grep pabot | wc -l` -lt 2 ];  then
+#    echo "pabot is not running-suite execution started"
+#    cd /home/fc/automation/AuQA
+#    sudo pabot --pabotlib -d Reports/cleanReports --output cleanReports.xml --variable environment:config118  /home/fc/automation/AuQA/Testcases/cleanReports.robot || echo $(date +%F_%H:%M:%S)- cleanReports failed >> Reports/executionLog.txt 2>&1
+#    sudo pabot --pabotlib --processes 2 --name "Guard1_General-test_$(date +%F_%H:%M:%S)" --reporttitle "BasicHotAbsoluteGuard" --outputdir Reports --output basichotGuard.xml --variable environment:config118  -v groupname:General-test -T /home/fc/automation/AuQA/Testcases/basicHotAbsoluteGuardTest.robot /home/fc/automation/AuQA/Testcases/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- BasicHotAbsoluteGuard/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
+#    sudo pabot --pabotlib --processes 2 --name "Guard2_Imputes-test_$(date +%F_%H:%M:%S)" --reporttitle "DeadSensorGuard" --outputdir Reports --output deadSensorGuard.xml --variable environment:config118  -v groupname:Imputes-test -T /home/fc/automation/AuQA/Testcases/deadSensorGuardTest.robot /home/fc/automation/AuQA/Testcases/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- DeadSensorGuard/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
+#    sudo pabot --pabotlib --processes 2 --name "Guard3_General-test_$(date +%F_%H:%M:%S)" --reporttitle "HotGuard" --outputdir Reports --output hotGuard.xml --variable environment:config118  -v groupname:General-test -T /home/fc/automation/AuQA/Testcases/hotGuardTest.robot  /home/fc/automation/AuQA/Testcases/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- HotGuard/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
+#    sudo rebot  --name "GuardTest" --reporttitle "Test performed on environment 118" --outputdir Reports --output output.xml /home/fc/automation/AuQA/Reports/basichotGuard.xml /home/fc/automation/AuQA/Reports/deadSensorGuard.xml /home/fc/automation/AuQA/Reports/hotGuard.xml 2>&1 | sudo tee -a Reports/executionLog.txt
+#    sudo pabot --pabotlib -d Reports/cleanReports --output moveReports.xml --variable environment:config118 /home/fc/automation/AuQA/Testcases/moveReports.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- moveReports failed >> Reports/executionLog.txt 2>&1
+#  else
+#    echo "pabot is already running"
+# fi
 
 ##Uncomment and use the below  for windows
 #cd E:/Ideavat/AutomatedQA/AuQA
