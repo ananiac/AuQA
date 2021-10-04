21cd2b4da499cf4cb52e6699e0e9ad84c6a05c91
ananiac <ananiac@ideavat.com>
2021-10-04 16:43:37 +0530

    attachment added

--- ExternalKeywords/sendemail.py
+++ ExternalKeywords/sendemail.py
@@ -7,9 +7,21 @@
    msg['To'] = 'drkirby@vigilent.com'
    # msg.set_content(" This is the test email sent from AUQA ")
 
+   def attach_file(file_name_with_path):
+      with open(file_name_with_path, "rb") as file:
+         file_content = file.read()
+         print("file data in binary", file_content)
+         file_name = file.name
+         print("file name", file_name)
+         msg.add_attachment(file_content, maintype="application", subtype=".html", filename=file_name)
+
    with open('emailTemplate.txt') as template:
       email_content=template.read()
       msg.set_content(email_content)
+
+   attach_file("Reports/report.html")
+   attach_file("Reports/log.html")
+   attach_file("Reports/output.xml")
 
    try:
       smtpObj = smtplib.SMTP('172.17.1.70', 25)
