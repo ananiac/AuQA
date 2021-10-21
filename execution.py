13a6153ba5a0230607ff726464d721801e84e649
ananiac <ananiac@ideavat.com>
2021-10-21 19:26:23 +0530

    updated execution py

--- execution.py
+++ execution.py
@@ -14,7 +14,7 @@ tc_path=dir_path+'/Testcases'
 gt_path=dir_path+'/Testcases/GuardTests'
 rp_path=dir_path+'/Reports'
 se_path=dir_path+'/ExternalKeywords'
-
+log_file=rp_path+'/executionLog.txt'
 #Suitename sent from the command line which is same as the excel sheet name for suite
 suite_name = sys.argv[1]
 
@@ -67,10 +67,11 @@ for i in range(dic_row):
 #Fetch the count of pabot process
 cmd = 'ps -ef | grep pabot | wc -l'
 pabot_output = subprocess.getstatusoutput(cmd)
-print(pabot_output)
+pabot_count = int(pabot_output[1])
+print("count of pabot process is: "+str(pabot_count))
 
 #check if the pabot process is not running and execute the commands
-if (pabot_output[1] <=str(2)):
+if (pabot_count <=2):
     print("No automated tests are running so starting the test execution")
     # Executing the testcases and redirecting the output to executionLog.txt
     for i in range(dic_row - 2):
@@ -79,7 +80,7 @@ if (pabot_output[1] <=str(2)):
             output_report = subprocess.getstatusoutput(command_for_execution[i])
         except Exception:
             print("Error in testcase execution")
-        with open("Reports/executionLog.txt", "a") as logfile:
+        with open(log_file, "a") as logfile:
             for line in output_report[1]:
                 fileout = logfile.write(line)
     #rebot command has to be separated from the loop of test execution else creates issue while combining the xml
@@ -90,7 +91,7 @@ if (pabot_output[1] <=str(2)):
             output_report = subprocess.getstatusoutput(command_for_execution[x])
         except Exception:
             print("Error in command execution")
-        with open("Reports/executionLog.txt", "a") as logfile:
+        with open(log_file, "a") as logfile:
             for line in output_report[1]:
                 fileout = logfile.write(line)
 else:
@@ -100,5 +101,4 @@ else:
 file_name=se_path+"/sendemail.py"
 print(file_name)
 call(["python3", file_name, suite_name])
-
 
