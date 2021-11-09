e475e41c95a72ac5a67af32b36785e8e641eb901
GreeshmaPK <greeshmap@ideavat.com>
2021-11-09 16:32:37 +0530

    'AuQA_66_Override1_testInput_added'

--- Configurations/config91.py
+++ Configurations/config91.py
@@ -7,7 +7,7 @@ graphql_base_url="https://10.252.9.91/api"
 #use headlessmode to run in ubuntu
 browser="headlesschrome"
 #use this to run on windows
-# browser="chrome"
+#browser="chrome"
 
 
 #Credentials to login
