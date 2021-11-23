df6f7188861c4993e450a56070b3ab88f3ef5187
abhijitvigs <abhijitd@ideavat.com>
2021-11-23 16:31:13 +0530

    AUQA-81_addExcelSheet1

--- thursdayexecutesuite.sh
+++ thursdayexecutesuite.sh
@@ -29,25 +29,3 @@ python3 execution.py thursdaysuite
 #  else
 #    echo "Automated test are running so the test execution is aborted"
 # fi
-
-#    ##Use the below command to execute the test individually in  Ubuntu machine
-#    sudo pabot --pabotlib -d Reports/cleanReports --output cleanReports.xml --variable environment:config118  /home/fc/automation/AuQA/Testcases/cleanReports.robot
-#    sudo pabot --pabotlib --processes 2 --name "Guard1_General-test_$(date +%F_%H:%M:%S)" --reporttitle "BasicHotAbsoluteGuard" --outputdir Reports --output basichotGuard.xml --variable environment:config118  -v groupname:General-test -T Testcases/GuardTests/basicHotAbsoluteGuardTest.robot Testcases/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- BasicHotAbsoluteGuard/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
-#    sudo pabot --pabotlib --processes 2 --name "Guard2_Imputes-test_$(date +%F_%H:%M:%S)" --reporttitle "DeadSensorGuard" --outputdir Reports --output deadSensorGuard.xml --variable environment:config118  -v groupname:Imputes-test -T Testcases/GuardTests/deadSensorGuardTest.robot Testcases/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- DeadSensorGuard/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
-#    sudo pabot --pabotlib --processes 2 --name "Guard3_General-test_$(date +%F_%H:%M:%S)" --reporttitle "HotGuard" --outputdir Reports --output hotGuard.xml --variable environment:config118  -v groupname:General-test -T Testcases/GuardTests/hotGuardTest.robot  Testcases/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- HotGuard/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
-#    sudo pabot --pabotlib --processes 2 --name "Guard4_General-test_$(date +%F_%H:%M:%S)" --reporttitle "GuardOrderMIX" --outputdir Reports --output guardOrderMIX.xml --variable environment:config118  -v groupname:General-test -T Testcases/GuardTests/guardOrderMIXTest.robot  Testcases/staleStatePrevention.robot >> Reports/executionLog.txt 2>&1 || echo $(date +%F_%H:%M:%S)- GuardOrderMIX/staleStatePrevention  execution failed >> Reports/executionLog.txt 2>&1
-#    sudo rebot  --name "GuardTest" --reporttitle "Test performed on environment 118" --outputdir Reports --output output.xml Reports/basichotGuard.xml Reports/deadSensorGuard.xml Reports/hotGuard.xml Reports/guardOrderMIX.xml 2>&1 | sudo tee -a Reports/executionLog.txt
-#    sudo pabot --pabotlib -d Reports/cleanReports --output moveReports.xml --variable environment:config118 /home/fc/automation/AuQA/Testcases/moveReports.robot
-#    python3 ExternalKeywords/sendemail.py thursdaydaysuite 2>&1 | tee -a Reports/executionLog.txt
-
-
-##Uncomment and use the below  for windows
-#cd E:/Ideavat/AutomatedQA/AuQA
-#pabot --pabotlib -d Reports/cleanReports --output cleanReports.xml --variable environment:config118  Testcases/cleanReports.robot
-#pabot --pabotlib --processes 2 --name "Guard1_General-test" --reporttitle "BasicHotAbsoluteGuard" --outputdir Reports --output basichotGuard.xml --variable environment:config118  -v groupname:General-test -T Testcases/GuardTests/basicHotAbsoluteGuardTest.robot Testcases/staleStatePrevention.robot
-#pabot --pabotlib --processes 2 --name "Guard2_Imputes-test" --reporttitle "DeadSensorGuard" --outputdir Reports --output deadSensorGuard.xml --variable environment:config118  -v groupname:Imputes-test -T Testcases/GuardTests/deadSensorGuardTest.robot Testcases/staleStatePrevention.robot
-#pabot --pabotlib --processes 2 --name "Guard3_General-test" --reporttitle "HotGuard" --outputdir Reports --output hotGuard.xml --variable environment:config118  -v groupname:General-test -T Testcases/GuardTests/hotGuardTest.robot  Testcases/staleStatePrevention.robot
-#pabot --pabotlib --processes 2 --name "Guard4_General-test" --reporttitle "GuardOrderMIX" --outputdir Reports --output guardOrderMIX.xml --variable environment:config118  -v groupname:General-test -T Testcases/GuardTests/guardOrderMIXTest.robot  Testcases/staleStatePrevention.robot
-#rebot --name "GuardTest" --reporttitle "Test performed on environment 118" --outputdir Reports --output output.xml Reports/basichotGuard.xml Reports/deadSensorGuard.xml Reports/hotGuard.xml Reports/guardOrderMIX.xml
-#pabot --pabotlib -d Reports/cleanReports --output moveReports.xml --variable environment:config118 Testcases/moveReports.robot
-
