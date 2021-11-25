3ae137a2965542c5c1cd163e64718b742ecbc4e6
abhijitvigs <abhijitd@ideavat.com>
2021-11-25 15:31:19 +0530

    AUQA-85_popupVerification1

---
+++ PageObjects/vxAIEngine.py
@@ -0,0 +1,14 @@
+# VX AIEngine - Site Editor Web Elements
+
+# VX AIEngine - Notifications tab
+# notifications_tab="id=tab-1306-btnInnerEl"
+# notifications_tab="xpath=//span[@id='tab-1306-btnInnerEl']"
+# notifications_tab="xpath=//span[@id='tab-1306-btnEl']/span[2]"
+# notifications_tab="xpath=//a[8]/span/span/span[2]"
+# notifications_tab="xpath=//span[contains(text(),'Notifications')]"
+notifications_tab="xpath=//span[contains(text(),'Notifications')]"
+
+suppress_alarm_button="xpath=//span[contains(text(),'Suppress Alarm')]"
+suppress_alarm_ok_button="xpath=//span[contains(text(),'OK')]"
+suppress_alarm_title="""xpath=//*[@id="messagebox-1001_header-title-textEl"]"""
+suppress_alarm_popup_message="""xpath=//*[@id="messagebox-1001-msg"]"""
