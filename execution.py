e5f3c173ba4de2dc8e85f42aebd2c749d9d3cfd9
ananiac <ananiac@ideavat.com>
2022-01-03 13:08:53 +0530

    AUQA-103_add_alarmtc_excel-Tc-commented

--- execution.py
+++ execution.py
@@ -73,15 +73,15 @@ for i in range(dic_row):
                 else:
                     execution_command = execution_command + " " + os.path.join(tc_path, command_input[i][h])
     command_for_execution.append(execution_command)
-    print(execution_command)
+    # print(execution_command)
 #Fetch the count of pabot process
 cmd = 'ps -ef | grep pabot | wc -l'
 pabot_output = subprocess.getstatusoutput(cmd)
-pabot_count = int(pabot_output[0])
+pabot_count = int(pabot_output[1])
 print("count of pabot process is: "+str(pabot_count))
 
 #check if the pabot process is not running and execute the commands
-if (pabot_count <=255):
+if (pabot_count <=2):
     print("No automated tests are running so starting the test execution")
     # Executing the testcases and redirecting the output to executionLog.txt
     for i in range(dic_row - 2):
